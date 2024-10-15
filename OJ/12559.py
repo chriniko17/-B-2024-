e376ac8191d3844ed70686e59275210138009add
chriniko17 <2400011425@stu.pku.edu.cn>
2024-10-15 11:01:44 +0800

    Changes

--- OJ/12559.py
+++ OJ/12559.py
@@ -1,10 +1,16 @@
 n=int(input())
 a=list(input().split())
 a.sort()
-print(a)
-b=a[::-1]
+lmax=max(len(a[i]) for i in range(n))
+c=[]
 for i in range(n):
-    print(b[i],end="")
+    temp=a[i]*int(lmax/len(a[i])+1)
+    c.append([a[i],temp[0:lmax:]])
+c.sort(key=lambda x:x[1])
+#print(c)
+b=c[::-1]
+for i in range(n):
+    print(b[i][0],end="")
 print(" ",end="")
 for i in range(n):
-    print(a[i],end="")
+    print(c[i][0],end="")
